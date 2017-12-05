aa0679ad8c7fd333f361d398f14a92454b374ef5
Oleg Illiashenko <illiashenkoo@digicode.net>
2017-12-05 09:34:08 +0200

    v 0.0.2

--- library/src/main/java/com/legacycle/ninepatchprogressbar/NinePatchProgressBar.java
+++ library/src/main/java/com/legacycle/ninepatchprogressbar/NinePatchProgressBar.java
@@ -8,8 +8,6 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.NinePatchDrawable;
 import android.os.Build;
-import android.support.annotation.DrawableRes;
-import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -25,6 +23,8 @@ public class NinePatchProgressBar extends View {
 
     private int bgPaddingTop;
     private int bgPaddingBottom;
+    private int bgPaddingLeft;
+    private int bgPaddingRight;
 
     public NinePatchProgressBar(Context context) {
         super(context);
@@ -41,70 +41,11 @@ public class NinePatchProgressBar extends View {
         init(context, attrs);
     }
 
-    private void init(Context context, AttributeSet attrs) {
-        initAttrs(context, attrs);
-        initPaths();
-    }
-
-    private void initAttrs(Context context, AttributeSet attrs) {
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.NinePatchProgressBar);
-        max = a.getInteger(R.styleable.NinePatchProgressBar_np_max, 100);
-        progress = a.getInteger(R.styleable.NinePatchProgressBar_np_progress, 0);
-
-        bgDrawable = (NinePatchDrawable) a.getDrawable(R.styleable.NinePatchProgressBar_np_bg_drawable);
-        pbDrawable = (NinePatchDrawable) a.getDrawable(R.styleable.NinePatchProgressBar_np_pb_drawable);
-
-        bgPaddingTop = a.getDimensionPixelSize(R.styleable.NinePatchProgressBar_np_bg_padding_top, 0);
-        bgPaddingBottom = a.getDimensionPixelSize(R.styleable.NinePatchProgressBar_np_bg_padding_bottom, 0);
-        a.recycle();
-    }
-
-    private void initPaths() {
-        if (bgDrawable == null) {
-            bgDrawable = (NinePatchDrawable) getDrawable(R.drawable.np_bg);
-        }
-
-        if (pbDrawable == null) {
-            pbDrawable = (NinePatchDrawable) getDrawable(R.drawable.np_pb);
-        }
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawBackground(canvas);
         drawProgress(canvas);
-    }
-
-    private void drawProgress(Canvas canvas) {
-        int width = getWidth();
-        if (width % 2 != 0) {
-            //Fix Me
-            width = width - 1;
-        }
-        float percent = 0;
-        if (max != 0) {
-            percent = progress * 1.0f / max;
-        }
-        int progressHeight = getHeight() - padding * 2;
-        if (progressHeight % 2 != 0) {
-            progressHeight = progressHeight - 1;
-        }
-        int progressWidth = width - padding * 2 - progressHeight;
-        float dx = progressWidth * percent;
-
-        Rect npdBounds = new Rect(0, padding, (int) (padding + progressHeight / 2 + dx), padding + progressHeight);
-        pbDrawable.setBounds(npdBounds);
-        pbDrawable.draw(canvas);
-    }
-
-    private void drawBackground(Canvas canvas) {
-        int height = getHeight();
-        int width = getWidth();
-
-        Rect npdBounds = new Rect(0, bgPaddingTop, width, height - bgPaddingBottom);
-        bgDrawable.setBounds(npdBounds);
-        bgDrawable.draw(canvas);
     }
 
     @Override
@@ -126,30 +67,10 @@ public class NinePatchProgressBar extends View {
     }
 
     public void setProgress(int progress) {
-//        Timber.d("progress: %s ", progress);
         this.progress = progress;
         invalidate();
     }
 
-//    public int getBgColor() {
-//        return bgColor;
-//    }
-//
-//    public void setBgColor(int bgColor) {
-//        this.bgColor = bgColor;
-//        bgPaint.setColor(bgColor);
-//        invalidate();
-//    }
-//
-//    public int getProgressColor() {
-//        return progressColor;
-//    }
-//
-//    public void setProgressColor(int progressColor) {
-//        this.progressColor = progressColor;
-//        progressPaint.setColor(progressColor);
-//        invalidate();
-//    }
 
     public int getPadding() {
         return padding;
@@ -160,11 +81,6 @@ public class NinePatchProgressBar extends View {
         invalidate();
     }
 
-    /**
-     * get the percentage value of progress and max.
-     *
-     * @return percentage value
-     */
     public int getPercentage() {
         if (max == 0) {
             return 0;
@@ -172,14 +88,86 @@ public class NinePatchProgressBar extends View {
         return (int) (progress * 100.0 / max);
     }
 
-    @Nullable
-    public final Drawable getDrawable(@DrawableRes int id) {
+    private void init(Context context, AttributeSet attrs) {
+        initAttrs(context, attrs);
+        initPaths();
+    }
+
+    private void initPaths() {
+        if (bgDrawable == null) {
+            bgDrawable = (NinePatchDrawable) getDrawable(R.drawable.np_bg);
+        }
+
+        if (pbDrawable == null) {
+            pbDrawable = (NinePatchDrawable) getDrawable(R.drawable.np_pb);
+        }
+    }
+
+    private void initAttrs(Context context, AttributeSet attrs) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.NinePatchProgressBar);
+
+        max = a.getInteger(R.styleable.NinePatchProgressBar_np_max, 100);
+        progress = a.getInteger(R.styleable.NinePatchProgressBar_np_progress, 0);
+
+        bgDrawable = (NinePatchDrawable) a.getDrawable(R.styleable.NinePatchProgressBar_np_bg_drawable);
+        pbDrawable = (NinePatchDrawable) a.getDrawable(R.styleable.NinePatchProgressBar_np_pb_drawable);
+
+        bgPaddingTop = a.getDimensionPixelSize(R.styleable.NinePatchProgressBar_np_bg_padding_top, 0);
+        bgPaddingBottom = a.getDimensionPixelSize(R.styleable.NinePatchProgressBar_np_bg_padding_bottom, 0);
+        bgPaddingLeft = a.getDimensionPixelSize(R.styleable.NinePatchProgressBar_np_bg_padding_left, 0);
+        bgPaddingRight = a.getDimensionPixelSize(R.styleable.NinePatchProgressBar_np_bg_padding_right, 0);
+
+        a.recycle();
+    }
+
+    private void drawProgress(Canvas canvas) {
+        int width = getWidth();
+        if (width % 2 != 0) {
+            width = width - 1;
+        }
+
+        float percent = getPercent();
+
+        int progressHeight = getHeight() - padding * 2;
+        if (progressHeight % 2 != 0) {
+            progressHeight = progressHeight - 1;
+        }
+
+        int progressWidth = width - padding * 2;
+        float dx = progressWidth * percent;
+
+        int right = (int) (padding + dx);
+        int bottom = padding + progressHeight;
+
+        Rect npdBounds = new Rect(0, padding, right, bottom);
+        pbDrawable.setBounds(npdBounds);
+        pbDrawable.draw(canvas);
+    }
+
+    private void drawBackground(Canvas canvas) {
+        int height = getHeight();
+        int width = getWidth();
+
+        int right = width - bgPaddingRight;
+        int bottom = height - bgPaddingBottom;
+
+        Rect npdBounds = new Rect(bgPaddingLeft, bgPaddingTop, right, bottom);
+
+        bgDrawable.setBounds(npdBounds);
+        bgDrawable.draw(canvas);
+    }
+
+    private Drawable getDrawable(int id) {
         if (Build.VERSION.SDK_INT >= 21) {
             return getContext().getDrawable(id);
         } else {
             Resources resources = getContext().getResources();
             return resources.getDrawable(id);
         }
+    }
+
+    private float getPercent() {
+        return max == 0 ? 0F : progress * 1.0F / max;
     }
 
 }
